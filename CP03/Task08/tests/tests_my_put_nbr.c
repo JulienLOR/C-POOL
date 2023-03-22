4d550e62225a834c5c58815261ec8cbf7509239a
Julien <julien.lor@epitech.eu>
2023-03-22 13:38:08 +0100

    Task done

---
+++ CP03/Task08/tests/tests_my_put_nbr.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** C-POOL
+** File description:
+** tests_my_put_nbr
+*/
+
+int main() {
+    my_isneg (0);
+    my_isneg (21);
+    my_isneg (-21);
+    return (0);
+}
